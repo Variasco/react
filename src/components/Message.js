1436f109dba86cb92b2b18f5f5873f989dfcd49a
variasco <vakin213@gmail.com>
2022-02-17 22:47:52 +0300

    hw2

---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+const Message = ({ item }) => {
+    return (
+        <div className="message">
+            <p className="message__author">{item.author}</p>
+            <p className="message__text">{item.text}</p>
+        </div>
+    )
+}
+
+export default Message;
